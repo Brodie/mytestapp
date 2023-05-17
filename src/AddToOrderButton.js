088332d909c89ecf9893b1dca4d7b00c3e091f0a
Brodie Ashcraft <brodiea19@gmail.com>
2023-05-17 10:49:55 -0700

    Moved Components to separate files

---
+++ src/AddToOrderButton.js
@@ -0,0 +1,12 @@
+const AddToOrderButton = () => {
+  const buttonText = "Add to Order!";
+  return (
+    <>
+      <button>{buttonText}</button>
+      <br />
+      <br />
+    </>
+  );
+};
+
+export default AddToOrderButton;
